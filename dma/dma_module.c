bbe6305b3b0912e13e7464a81b11f0116e564448
Altus <23116982+altuSemi@users.noreply.github.com>
2018-11-28 20:43:38 -0500

    Add files via upload

--- dma/dma_module.c
+++ dma/dma_module.c
@@ -23,27 +23,44 @@
 
 DMACtrlBuf DMAControlBuffer={
 			0,		//Control Descriptor
-			(uint32_t) 0,	//Control Pointer
- 			0x04040000,	//Control Physical Address
+			NULL,		//Control Pointer
+ 			CONTROL_PHY_ADD,//Control Physical Address
 			0,		//Buf select 0/1
 			0,		//Buf length
 			0,		//Buffer0 Descriptor
 			0,		//Buffer1 Descriptor
-			(uint32_t) 0,	//Buffer0 Pointer
-			(uint32_t) 0, 	//Buffer1 Pointer
-			0x01000000,	//Buffer0 Physical Address
-			0x02000000	//Buffer1 Physical Address
+			NULL,		//Buffer0 Pointer
+			NULL	, 	//Buffer1 Pointer
+			BUFFER0_PHY_ADD,//Buffer0 Physical Address
+			BUFFER1_PHY_ADD //Buffer1 Physical Address
 };
+
+
+/* Create a function that can free our pointers */
+static PyObject * DMALib_DRAMFreeMem(PyObject *self, PyObject *args)
+{
+    munmap(DMAControlBuffer.CtrlMMAPPointer,65535);
+    munmap(DMAControlBuffer.BufMMAPPointer[0],65535);
+    munmap(DMAControlBuffer.BufMMAPPointer[1],65535);
+    return Py_BuildValue("i", true);;
+}
+
+static PyObject * DMALib_DRAMMMAP(PyObject *self, PyObject *args)
+{
+    //DMA control and buffers struct init:
+    DMAControlBuffer.CtrlDescriptor = open("/dev/uio0", O_RDWR | O_SYNC); // Open /dev/uio0 which represents the dma control area
+    DMAControlBuffer.CtrlMMAPPointer = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED,DMAControlBuffer.CtrlDescriptor, 0); // Memory map AXI Lite register block
+    //Buffers are inputs, not from UIO
+    DMAControlBuffer.BufDescriptor[0] = open("/dev/uio1", O_RDWR | O_SYNC); // Open /dev/uio1 which represents the dma source buffer
+    DMAControlBuffer.BufMMAPPointer[0]  = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED, DMAControlBuffer.BufDescriptor[0], 0); // Memory map source address
+    DMAControlBuffer.BufDescriptor[1] = open("/dev/uio2", O_RDWR | O_SYNC); // Open /dev/uio2 which represents the dma destination buffer
+    DMAControlBuffer.BufMMAPPointer[1] = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED, DMAControlBuffer.BufDescriptor[1], 0); // Memory
+    return Py_BuildValue("i", true);;
+}
 
 static PyObject * DMALib_DMABufGet(PyObject *self, PyObject *args) {
 
   uint32_t * DstPointer=DMAControlBuffer.BufMMAPPointer[!DMAControlBuffer.BufSelect];
-  //int *array = NULL;
-  int i;
-
-  //if (!(array = malloc(DMAControlBuffer.BufLength * sizeof(int)))) return NULL;
-  //for (i = 0; i < DMAControlBuffer.BufLength; ++i) array[i] = DstPointer[i];
-  // return the array as a numpy array (numpy will free it later)
   npy_intp dims[1] = {DMAControlBuffer.BufLength};
   PyObject *narray = PyArray_SimpleNewFromData(1, &dims[0], NPY_INT, (int*)DstPointer);
   // this is the critical line - tell numpy it has to free the data
@@ -67,7 +84,10 @@ static PyObject *DMALib_DMABufSet(PyObject *self, PyObject *args) {
     if (n < 0) {
         return NULL;
     }
-    bool SrcBuf=0;
+    if (n > 16000) {
+        return NULL;
+    }
+
     uint32_t * SourcePointer=DMAControlBuffer.BufMMAPPointer[DMAControlBuffer.BufSelect];
     for (int i = 0; i < n; i++) {
         PyLongObject *item = PyList_GetItem(lst, i);
@@ -81,11 +101,20 @@ static PyObject *DMALib_DMABufSet(PyObject *self, PyObject *args) {
 static PyObject *DMALib_DMATransfer(PyObject *self, PyObject *args) {  
     DMATransfer();
     return Py_BuildValue("i", true);
-    //return PyLong_FromVoidPtr(nums_out);
 }
 
 static PyMethodDef DMALib_FunctionsTable[] = {  
-    {
+      {
+        "DRAMFreeMem",      // name exposed to Python
+        DMALib_DRAMFreeMem, // C wrapper function
+        METH_VARARGS,          // received variable args (but really just 1)
+        "Free memory buffers" // documentation
+    },{
+        "DRAMMMAP",      // name exposed to Python
+        DMALib_DRAMMMAP, // C wrapper function
+        METH_VARARGS,          // received variable args (but really just 1)
+        "Map memory buffers" // documentation
+    },{
         "DMABufSwap",      // name exposed to Python
         DMALib_DMABufSwap, // C wrapper function
         METH_VARARGS,          // received variable args (but really just 1)
@@ -124,7 +153,7 @@ PyMODINIT_FUNC PyInit_dma(void) {
     import_array();
     //DMA control and buffers struct init:
     DMAControlBuffer.CtrlDescriptor = open("/dev/uio0", O_RDWR | O_SYNC); // Open /dev/uio0 which represents the dma control area
-    DMAControlBuffer.CtrlMMAPPointer = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED,DMAControlBuffer.CtrlDescriptor, 0); // Memory map AAXI Lite register block
+    DMAControlBuffer.CtrlMMAPPointer = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED,DMAControlBuffer.CtrlDescriptor, 0); // Memory map AXI Lite register block
     //Buffers are inputs, not from UIO
     DMAControlBuffer.BufDescriptor[0] = open("/dev/uio1", O_RDWR | O_SYNC); // Open /dev/uio1 which represents the dma source buffer
     DMAControlBuffer.BufMMAPPointer[0]  = mmap(NULL, 65535, PROT_READ | PROT_WRITE, MAP_SHARED, DMAControlBuffer.BufDescriptor[0], 0); // Memorymap source address
